cad3e6fe29e43dc62f7ebf75e6af691b43cb9b34
voznyukmd <voznyuk.md@gmail.com>
2016-06-10 11:15:26 +0300

    Change var names

--- main.js
+++ main.js
@@ -25,9 +25,9 @@ memory_array [14] = 'aqua';
 memory_array [15] = 'aqua';
 
 var memory_values = [];
-var memory_tile_ids = [];
-var tiles_flipped = 0;
-Array.prototype.memory_tile_shuffle = function(){
+var memory_color_ids = [];
+var colors_flipped = 0;
+Array.prototype.memory_color_shuffle = function(){
     var i = this.length, j, temp;
     while(--i > 0){
         j = Math.floor(Math.random() * (i+1));
@@ -37,49 +37,49 @@ Array.prototype.memory_tile_shuffle = function(){
     }
 }
 function newBoard(){
-	tiles_flipped = 0;
+	colors_flipped = 0;
 	var output = '';
-    memory_array.memory_tile_shuffle();
+    memory_array.memory_color_shuffle();
 	for(var i = 0; i < memory_array.length; i++){
-		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+		output += '<div id="color_'+i+'" onclick="memoryFlipColor(this,\''+memory_array[i]+'\')"></div>';
 
 	}
 	document.getElementById('memory_board').innerHTML = output;
 
 }
-function memoryFlipTile(tile,val){
-	if(tile.innerHTML == "" && memory_values.length < 2){
-		tile.style.background = val;
-		tile.innerHTML = val;
+function memoryFlipColor(color,val){
+	if(color.innerHTML == "" && memory_values.length < 2){
+		color.style.background = val;
+		color.innerHTML = val;
 		if(memory_values.length == 0){
 			memory_values.push(val);
-			memory_tile_ids.push(tile.id);
+			memory_color_ids.push(color.id);
 		} else if(memory_values.length == 1){
 			memory_values.push(val);
-			memory_tile_ids.push(tile.id);
+			memory_color_ids.push(color.id);
 			if(memory_values[0] == memory_values[1]){
-				tiles_flipped += 2;
+				colors_flipped += 2;
 				// Clear both arrays
 				memory_values = [];
-        memory_tile_ids = [];
+        memory_color_ids = [];
 				// Check to see if the whole board is cleared
-				if(tiles_flipped == memory_array.length){
+				if(colors_flipped == memory_array.length){
 					alert("Вітаю! Ви це зробили. Cпробуйте знову.");
 					document.getElementById('memory_board').innerHTML = "";
 					newBoard();
 				}
 			} else {
 				function flip2Back(){
-				    // Flip the 2 tiles back over
-				    var tile_1 = document.getElementById(memory_tile_ids[0]);
-				    var tile_2 = document.getElementById(memory_tile_ids[1]);
-				    tile_1.style.background = 'no-repeat';
-            	    tile_1.innerHTML = "";
-				    tile_2.style.background = 'no-repeat';
-            	    tile_2.innerHTML = "";
+				    // Flip the 2 colors back over
+				    var color_1 = document.getElementById(memory_color_ids[0]);
+				    var color_2 = document.getElementById(memory_color_ids[1]);
+				    color_1.style.background = 'no-repeat';
+            	    color_1.innerHTML = "";
+				    color_2.style.background = 'no-repeat';
+            	    color_2.innerHTML = "";
 				    // Clear both arrays
 				    memory_values = [];
-            memory_tile_ids = [];
+            memory_color_ids = [];
 				}
 				setTimeout(flip2Back, 900);
 			}
